65b094d84e99f926d3fdcac380b1cf7627b42b8c
Uddhav <uddhavlk@gmail.com>
2023-12-27 23:29:01 +0545

    Loading icon added

--- src/components/FetchDetail.jsx
+++ src/components/FetchDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import DetailsTable from "./DetailsTable";
+import { FaSpinner } from "react-icons/fa";
 
 const FetchDetail = () => {
   const { id } = useParams();
