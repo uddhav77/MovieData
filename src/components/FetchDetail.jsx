3f93c5ea8396c586417b672c9db66c9d28895dfb
Uddhav <uddhavlk@gmail.com>
2023-12-27 23:24:12 +0545

    Loading state added

--- src/components/FetchDetail.jsx
+++ src/components/FetchDetail.jsx
@@ -5,6 +5,7 @@ import DetailsTable from "./DetailsTable";
 const FetchDetail = () => {
   const { id } = useParams();
   const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
   const fetchData = async () => {
     try {
       const response = await fetch(
@@ -13,13 +14,24 @@ const FetchDetail = () => {
       const value = await response.json();
       console.log(value);
       setData(value);
+      setLoading(false);
     } catch (err) {
       console.log("Error occured");
+      setLoading(false);
     }
   };
   useEffect(() => {
     fetchData();
   }, [id]);
+
+  if (loading) {
+    return (
+      <div className="h-screen flex flex-col items-center justify-center">
+        <FaSpinner className="animate-spin" size={40} />
+        <p className="mt-4 text-gray-600">Loading Movies...</p>
+      </div>
+    );
+  }
   return (
     <div className="flex justify-center">
       <DetailsTable data={data} />
